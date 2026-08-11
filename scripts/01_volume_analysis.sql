d5504bd1cb96a566404e3b1b36fe1d829359e979
faithkilango <102891689+faithkilango@users.noreply.github.com>
2026-08-11 23:56:59 +0300

    Add overview comment to volume analysis SQL script
    Added an overview comment explaining the purpose of the query.

--- scripts/01_volume_analysis.sql
+++ scripts/01_volume_analysis.sql
@@ -1,1 +1,13 @@
+/*
+The Overview (Vol & Stats)
+Goal: How much work do we have? Understanding the total workload and the state of tickets
+Why: Tells us if the support team is overwhelmed
+*/
+
+SELECT
+  status,
+  COUNT(ticket_id) AS total_tickets,
+  ROUND(COUNT(*) *100.0 / SUM(COUNT(*)) OVER (), 2) AS pct_of_total
+FROM `helpdesk_data.tickets`
+GROUP BY status;
 
